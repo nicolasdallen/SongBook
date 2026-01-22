03ab42471438dea3fa2618d7fa54d9392736d566
nicolasdallen <64791064+nicolasdallen@users.noreply.github.com>
2026-01-22 14:17:55 +0100

    Add ImportScreen widget

---
+++ lib/screens/import_screen.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class ImportScreen extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Import Screen'),
+      ),
+      body: Center(
+        child: Text('Welcome to the Import Screen!'),
+      ),
+    );
+  }
+}
